5f8aa8f6c779bcc77d83329983f2f30fe3a62c67
Roman Geathers <romangeathersdev@gmail.com>
2022-09-15 20:02:51 -0400

    Continue work on player interaction + display
    Allow players to go back and forth placing symbols on board. Fill
    in game board array appropriately. Continue work on announcing
    win conditions being fulfilled. Current bug exists in alerting correct
    winner symbol.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+const boardHTML = document.querySelector(".game-board");
 const topRow = document.querySelectorAll('.top-row-location');
 const midRow = document.querySelectorAll('.mid-row-location');
 const bottomRow = document.querySelectorAll('.bottom-row-location');
 
 const gameBoard = (() => {
   let gameBoardArr = [
-    ['x', 'x', 'o'], 
-    ['x', 'x', 'o'], 
-    ['x', 'x', 'o']];
+    [' ', ' ', ' '], 
+    [' ', ' ', ' '], 
+    [' ', ' ', ' ']];
 
   const updateBoardLocation = (rowLocation, gameBoardArr, row, column) => {
-    rowLocation[column].innerHTML = `<p>${gameBoardArr[row][column]}</p>`;
+    rowLocation[column].textContent = `${gameBoardArr[row][column]}`;
   };
 
   const renderBoard  = () => {
@@ -27,16 +28,80 @@ const gameBoard = (() => {
   };
 
   return {
-    renderBoard
+    renderBoard,
+    gameBoardArr
   };
 })();
 
-const displayController = (() => {
-
-})();
-
-const Player = () => {
-  return;
+const Player = (number, symbol) => {
+  
+  return {
+    number,
+    symbol
+  };
 };
 
+const p1Symbol = prompt('Player 1, do you want to be \'X\' or \'O\'?');
+let player1 = Player(1, p1Symbol);
+let player2 = null;
+
+if (p1Symbol === 'x') {
+  player2 = Player(2, 'o');
+} else {
+  player2 = Player(2, 'x');
+}
+
+const displayController = (() => {
+  let currentSymbol = player1.symbol;
+
+  boardHTML.addEventListener('click', (e) => {
+    selectedRow = e.target.getAttribute("data-row");
+    selectedColumn = e.target.getAttribute("data-column");
+
+    if (currentSymbol === player1.symbol) {
+      gameBoard.gameBoardArr[selectedRow][selectedColumn] = player1.symbol;
+      currentSymbol = player2.symbol;
+    } else {
+      gameBoard.gameBoardArr[selectedRow][selectedColumn] = player2.symbol;
+      currentSymbol = player1.symbol;
+    }
+    gameBoard.renderBoard();
+    checkWinner();
+  });
+
+  const checkWinner = () => {
+    gameBoard.gameBoardArr.forEach((row) => {
+      for (let i = 0; i < 3; i++) {
+        if (row[i] === row[i + 1] && row[i] === row[i + 2] && row[i] !== ' ') {
+          if (row[i] === player1.symbol) {
+            alert(`Player 1 (${player1.symbol} wins)!`);
+          } else {
+            alert(`Player 2 (${player2.symbol} wins)!`);
+          }
+        }
+      }
+    });
+
+    for (let i = 0; i < 3; i++) {
+      if (gameBoard.gameBoardArr[0][i] === gameBoard.gameBoardArr[1][i] && 
+          gameBoard.gameBoardArr[0][i] === gameBoard.gameBoardArr[2][i] &&
+          gameBoard.gameBoardArr[0][i] !== ' ') {
+        if (gameBoard.gameBoardArr[i][0] === player1.symbol) {
+          alert(`Player 1 (${currentSymbol} wins)!`);
+        } else {
+          alert(`Player 2 (${currentSymbol} wins)!`);
+        }
+        // but alerting correct winner symbol
+      }
+    }
+  };
+})();
+
 gameBoard.renderBoard();
+
+// let runGame = true;
+// let turn;
+
+// while (runGame) {
+//   turn = player1.number;
+// }
